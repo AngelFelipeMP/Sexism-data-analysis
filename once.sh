031486b36f7b50840ded32e10bc149a321ccb723
Angel de Paula <51416519+AngelFelipeMP@users.noreply.github.com>
2023-07-22 15:04:42 +1000

    Create once.sh

---
+++ once.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+ebs_path="/dev/nvme1n1"
+
+# S3-bucket
+sudo apt update -y
+sudo apt install s3fs -y
+mkdir s3-bucket
+
+# EBS volume
+sudo file -s $ebs_path
+sudo mkfs -t xfs $ebs_path
+mkdir ebs-volume
+
+## SSD internal disck
+mkdir ssd-volume
