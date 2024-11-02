c199c950a7627e0ef56db2008f074eb218fba1ac
Angel Felipe Magnossao de Paula <angel.magnossao@gmail.com>
2024-11-02 13:37:55 +1100

    process llms outputs - v1

--- LLM_output_verification.py
+++ LLM_output_verification.py
@@ -5,19 +5,18 @@ import re
 from difflib import get_close_matches
 from config import PREDICTIONS_PATH, CATEGORIES_TASK1, CATEGORIES_TASK2, CATEGORIES_TASK3
 
-# path = 'gpt-3.5-turbo-0125_ZeroShotTask{number}.tsv'
-
 def output_report():
-    preds_list = os.listdir(PREDICTIONS_PATH)
+    preds_list =  sorted(os.listdir(PREDICTIONS_PATH))
 
-    for preds in preds_list:
-        print(f'PRED FILE: {preds}')
-        file_path = os.path.join(PREDICTIONS_PATH, preds)
-        df = pd.read_csv(file_path, sep='\t')
+    for preds_file in preds_list:
+        print(f'PRED FILE: {preds_file}')
+        file_path = os.path.join(PREDICTIONS_PATH, preds_file)
+        df = pd.read_csv(file_path, sep='\t', index_col='id_EXIST')
         
-        llm = preds.split('_')[0]
-        
-        if 'Task3' in preds: 
+        llm = preds_file.split('_')[0]
+        llm = llm+'_processed' if '_processed' in preds_file else llm
+            
+        if 'Task3' in preds_file: 
             list_outputs = [ast.literal_eval(output) for output in df[llm].to_list()]
             flattened_list = [item for sublist in list_outputs for item in sublist]
             print(list(set(flattened_list)))
@@ -47,46 +46,51 @@ def add_UNKNOWN_categories(llm_output):
     else:
         return 'UNKNOWN'
     
-
+def add_category_no_sexist(pred, task):
+    if isinstance(pred, list):
+        return pred
+    else:
+        if pd.isna(pred):
+            if 'Task2' in task:
+                return '-'
+            elif 'Task3' in task:
+                return ['-']
+        else:
+            return pred
+    
+    
 def process_predictions():
     preds_list = [file for file in os.listdir(PREDICTIONS_PATH) if '_processed' not in file]
+    preds_list = sorted(preds_list, key=lambda x: (0 if 'Task1' in x else 1, x))
     
     # preds_list = ['gpt-4o-2024-08-06_ZeroShotTask1.tsv']
 
-    for preds in preds_list:
-        file_path = os.path.join(PREDICTIONS_PATH, preds)
+    for preds_file in preds_list:
+        print(f'PROCESSING FILE: {preds_file}')
+        file_path = os.path.join(PREDICTIONS_PATH, preds_file)
         df = pd.read_csv(file_path, sep='\t', index_col='id_EXIST')
         
-        llm = preds.split('_')[0]
+        llm = preds_file.split('_')[0]
         
         df[llm+'_processed'] = df[llm].apply(lambda x: '' if isinstance(x, float) else x)
-        categories_task = CATEGORIES_TASK1 if 'Task1' in preds else CATEGORIES_TASK2 if 'Task2' in preds else CATEGORIES_TASK3
+        categories_task = CATEGORIES_TASK1 if 'Task1' in preds_file else CATEGORIES_TASK2 if 'Task2' in preds_file else CATEGORIES_TASK3
         df[llm+'_processed'] = df[llm+'_processed'].apply(lambda x: process_categories(categories_task, x))
         df[llm+'_processed'] = df[llm+'_processed'].apply(lambda x: add_UNKNOWN_categories(x))
         
-        if 'Task2' in preds or 'Task3' in preds:
-            task1_file = preds.split('.tsv')[0][:-1] + '1' + '_processed.tsv'
+        if 'Task2' in preds_file or 'Task3' in preds_file:
+            task1_file = preds_file.split('.tsv')[0][:-1] + '1' + '_processed.tsv'
             df_task1 = pd.read_csv(os.path.join(PREDICTIONS_PATH, task1_file), sep='\t', index_col='id_EXIST')
             df_task1 = df_task1.loc[df_task1[llm+'_processed']=='NO',['lang']]
             
-            
-            print(len(df))
-            print(len(df_task1))
             df = pd.concat([df,df_task1])
-            print(len(df))
-            
-            ## STOPED HERE !!!!
-            df = df.fillna('-') if 'Task2' in preds else df.fillna(['-'])
-            
-            print(df_task1.head())
-            print(df.tail())
-            exit()
+
+            df[llm+'_processed'] = df[llm+'_processed'].apply(lambda x: add_category_no_sexist(x, preds_file))
+            df = df.sort_index()
         
-        save_path = os.path.join(PREDICTIONS_PATH, preds.split('.tsv')[0] + '_processed.tsv')
-        # print(f'SAVED: {save_path}')
-        # exit()
-        df.to_csv(save_path, sep='\t', index=False)
+        save_path = os.path.join(PREDICTIONS_PATH, preds_file.split('.tsv')[0] + '_processed.tsv')
+        df.to_csv(save_path, sep='\t')
 
 if __name__ == "__main__":
+    output_report()
     process_predictions()
-    # output_report()
+    output_report()
